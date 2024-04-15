5d0686d39de68d1386206d0d7ba7c2f26771a2be
nagmo01 <bjworkshop01@gmail.com>
2024-04-15 21:51:51 +0900

    editButtonno

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState } from "react"
-import { GrUpdate } from "react-icons/gr";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { FaPen } from "react-icons/fa";
 
 let count = 0;
 
@@ -86,8 +86,8 @@ function App() {
             <p className='text-sm'>{todo.text}</p>
           </div>
           <div className='mr-3 flex items-center'>
-            <button className='rounded-full bg-green-400 p-2 text-white mr-1' onClick={() => onEdit(todo.id)}>
-              <GrUpdate className='w-3 h-3' />
+            <button className='rounded-full bg-white hover:bg-black p-2 text-black hover:text-white border border-black mr-1' onClick={() => onEdit(todo.id)}>
+              <FaPen className='w-3 h-3' />
             </button>
             <button className='rounded-full bg-black p-2 text-white' onClick={() => onDelete(todo.id)}>
               <FaRegTrashAlt className='w-3 h-3' />
@@ -101,7 +101,7 @@ function App() {
         <div className='flex justify-center mt-1' key={todo.id}>
           <input className='w-2/3 border border-gray rounded me-1 my-2 text-xs pl-1' type="text" value={editValue} onChange={onEditChange} />
           <button className="text-xs text-white self-center rounded-md bg-red-400 py-1 my-2 px-2" onClick={() => onUpdate(todo.id)} >
-            update
+            Enter
           </button>
         </div>
       );
@@ -109,18 +109,18 @@ function App() {
 
   return (
     <div className='max-w-sm bg-white mx-auto rounded-md shadow-lg mt-10'>
-      <div className='flex justify-between pt-3 border-4 border-blue-300'>
+      <div className='flex justify-between pt-3'>
         <h1 className='font-bold text-xl ml-3'>Tasks</h1>
         <p className="text-xs text-gray-500 mr-3 self-center">You have {todos.length} tasks</p>
       </div>
 
-      <div className='flex justify-center pt-5 pb-5 border-4 border-green-300'>
+      <div className='flex justify-center pt-5 pb-5'>
         <input className='pl-1 text-xs w-2/3 border border-gray border-1 rounded' type="text" value={value} onChange={onChange} placeholder='Add a new task' maxLength={20} />
         <button className="text-white rounded-md bg-black px-3 ml-1 pb-1" onClick={addTodo} >+</button>
       </div>
 
 
-      <div className='text-2xl container mx-auto pb-2 border-4 border-red-300'>
+      <div className='text-2xl container mx-auto pb-2'>
           {list}
       </div>
 
