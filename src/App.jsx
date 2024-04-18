a06c4e3bf15b31abd4e025b08f1153f3d96aff8f
nagmo01 <bjworkshop01@gmail.com>
2024-04-18 16:26:18 +0900

    2カラムに変更

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 import { useState } from "react";
 import Typical from "react-typical";
 import { FaRegTrashAlt } from "react-icons/fa";
@@ -7,101 +7,120 @@ import { FaPen } from "react-icons/fa";
 let count = 0;
 
 function App() {
-
-
   const [todos, setTodos] = useState([]);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState("");
 
-  const [editValue, setEditValue] = useState('');
-
+  const [editValue, setEditValue] = useState("");
 
   const addTodo = () => {
-    if(value == '' ) {
+    if (value == "") {
       return;
     }
 
-    const newTodos = [...todos, { id: count, text: value, type: "task", completed: false}]
-    setTodos(newTodos)
-    setValue('')
+    const newTodos = [
+      ...todos,
+      { id: count, text: value, type: "task", completed: false },
+    ];
+    setTodos(newTodos);
+    setValue("");
     count += 1;
-  }
+  };
 
   const onUpdate = (id) => {
-    setTodos(todos.map((todo) => {
-      if (todo.id === id) {
-        return { ...todo, type: "task", text: editValue }
-      } else {
-        return { ...todo }
-      }
-    }))
-  }
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, type: "task", text: editValue };
+        } else {
+          return { ...todo };
+        }
+      })
+    );
+  };
 
   const onChange = (e) => {
-    setValue(e.target.value)
-  }
+    setValue(e.target.value);
+  };
 
   const onEditChange = (e) => {
-    setEditValue(e.target.value)
-  }
-
+    setEditValue(e.target.value);
+  };
 
   const onDelete = (id) => {
-    setTodos(todos.filter((todo) => todo.id !== id))
-  }
-
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
 
   const onEdit = (id) => {
     setTodos(
       todos.map((todo) => {
-        if (todo.id === id ) {
+        if (todo.id === id) {
           setEditValue(todo.text);
-          return { ...todo, type: "form"};
+          return { ...todo, type: "form" };
         } else {
-          return { ...todo, type: "task"};
+          return { ...todo, type: "task" };
         }
       })
-    )
-  }
+    );
+  };
 
   const onCheck = (id) => {
-    setTodos(todos.map((todo) => 
-      todo.id === id ?
-        {...todo, completed: !todo.completed }
-        :
-        {...todo}
-    ))
-  }
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : { ...todo }
+      )
+    );
+  };
 
-  const list = todos.map((todo) => {
+  const list = todos.map((todo, index) => {
     return todo.type === "task" ? (
       //デフォルト
       <div key={todo.id}>
-        <div className='mx-2 my-2 flex justify-between'>
-          <div className='flex justify-start items-center'>
-            <input 
-              className='ml-2 mr-1 form-checkbox' 
-              type="checkbox" 
-              checked={todo.completed} 
-              onChange={() => onCheck(todo.id)} 
+        {index === 0 ? (
+          <h1 className="custom-border-red text-xs font-bold text-white shadow custom-bg-black ps-2 py-1 rounded-sm">
+            7月
+          </h1>
+        ) : (
+            <h1></h1>
+          )}
+        <div className="mx-1 my-2 flex justify-between border shadow rounded py-1">
+          <div className="flex justify-start items-center">
+            <input
+              className="ml-2 mr-1 form-checkbox"
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => onCheck(todo.id)}
             />
-            <p className='text-sm'>{todo.text}</p>
+            <p className="text-sm">{todo.text}</p>
           </div>
-          <div className='mr-3 flex items-center'>
-            <button className='rounded-full bg-white hover:bg-black p-2 text-black hover:text-white border border-black mr-1' onClick={() => onEdit(todo.id)}>
-              <FaPen className='w-3 h-3' />
+          <div className="mr-3 flex items-center">
+            <button
+              className="rounded-full bg-white hover:bg-black p-2 text-black hover:text-white border border-black mr-1"
+              onClick={() => onEdit(todo.id)}
+            >
+              <FaPen className="w-3 h-3" />
             </button>
-            <button className='rounded-full bg-black p-2 text-white' onClick={() => onDelete(todo.id)}>
-              <FaRegTrashAlt className='w-3 h-3' />
+            <button
+              className="rounded-full bg-black p-2 text-white"
+              onClick={() => onDelete(todo.id)}
+            >
+              <FaRegTrashAlt className="w-3 h-3" />
             </button>
           </div>
-
         </div>
       </div>
     ) : (
         //更新画面
-        <div className='flex justify-center mt-1' key={todo.id}>
-          <input className='w-2/3 border border-gray rounded me-1 my-2 text-xs pl-1' type="text" value={editValue} onChange={onEditChange} />
-          <button className="text-xs text-white self-center rounded-md bg-red-400 py-1 my-2 px-2" onClick={() => onUpdate(todo.id)} >
+        <div className="flex justify-center mt-1" key={todo.id}>
+          <input
+            className="w-2/3 border border-gray rounded me-1 my-2 text-xs pl-1"
+            type="text"
+            value={editValue}
+            onChange={onEditChange}
+          />
+          <button
+            className="text-xs text-white self-center rounded-md bg-red-400 py-1 my-2 px-2"
+            onClick={() => onUpdate(todo.id)}
+          >
             Enter
           </button>
         </div>
@@ -109,24 +128,45 @@ function App() {
   });
 
   return (
-    <div className='max-w-sm bg-white mx-auto rounded-md shadow-lg mt-10'>
-      <div className='flex justify-between pt-3'>
-        <h1 className='font-bold text-xl ml-3'>Tasks</h1>
-        <p className="text-xs text-gray-500 mr-3 self-center">You have {todos.length} tasks</p>
-      </div>
-
-      <div className='flex justify-center pt-5 pb-5'>
-        <input className='pl-1 text-xs w-2/3 border border-gray border-1 rounded' type="text" value={value} onChange={onChange} placeholder='Add a new task' maxLength={20} />
-        <button className="text-white rounded-md bg-black px-3 ml-1 pb-1" onClick={addTodo} >+</button>
-      </div>
+    <div className="w-4/5 mx-auto rounded-md mt-10 flex">
 
 
-      <div className='text-2xl container mx-auto pb-2'>
-          {list}
+      <div className="w-full flex justify-center">
+        <div className="pt-32 w-full">
+          <h1 class="text-center font-bold font-serif custom-black mb-5">Add a new task</h1>
+          <div className="flex bg-white rounded-md shadow-md justify-center pt-5 pb-5">
+            <input
+              className="pl-1 text-xs w-2/3 border border-gray border-1 rounded"
+              type="text"
+              value={value}
+              onChange={onChange}
+              placeholder="Add a new task"
+              maxLength={20}
+            />
+            <button
+              className="text-white rounded-md bg-black px-3 ml-1 pb-1"
+              onClick={addTodo}
+            >
+              +
+            </button>
+          </div>
+        </div>
       </div>
+
+
+      <div className="min-h-96 text-2xl container ms-4 pb-2 bg-white shadow flex-shrink">
+        <div className="flex justify-between py-3">
+          <h1 className="font-bold text-xl ml-3">Tasks</h1>
+          <p className="text-xs text-gray-500 mr-3 self-center">
+            You have {todos.length} tasks
+          </p>
+        </div>
+        {list}
+      </div>
+
 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
